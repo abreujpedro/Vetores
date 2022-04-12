ba9e7b7a1b6eeb23804b9ab3c480f8f6109de832
João Pedro Abreu <jp.dellabianca@gmail.com>
2022-04-12 10:41:55 -0300

    Add animation

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ import animate from './render.js';
 const test = new Graphs();
 test.doLine([[100], [200]], [[200], [200]]);
 
-// console.table(test.lines[0]);
-// setInterval(() => {
-//   test.rotateLine(0, 50, [[150], [200]]);
-//   console.table(test.lines[0]);
-// }, 600);
+console.table(test.lines[0]);
+setInterval(() => {
+  test.rotateLine(0, 45, [[150], [200]]);
+  console.table(test.lines[0]);
+}, 600);
 
 animate(test);
